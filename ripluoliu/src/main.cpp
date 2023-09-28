6f4c7fe533833c640087f139a32e85c5736e2278
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2023-09-28 21:33:14 +0200

    Parse command line arguments.

--- ripluoliu/src/main.cpp
+++ ripluoliu/src/main.cpp
@@ -36,6 +36,7 @@
 #include <iostream>
 
 #include <cs/IO/File.h>
+#include <cs/Text/StringAlgorithm.h>
 
 #include "block.h"
 #include "fourcc.h"
@@ -65,10 +66,63 @@ void extractStream(const std::filesystem::path& output, const cs::Buffer& buffer
 
 ////// Main //////////////////////////////////////////////////////////////////
 
+const char *arg_filename = NULL;
+FourCC arg_fourcc;
+
+bool parseArgs(const int argc, char **argv)
+{
+  arg_filename = NULL;
+  arg_fourcc.fill('\0');
+
+  int opt = 1;
+  for( ; opt < argc; opt++ ) {
+    if( argv[opt][0] != '-' ) {
+      break;
+    }
+
+    if( cs::startsWith(argv[opt], "--rip=") ) {
+      const char *opt_fourcc = &argv[opt][6];
+
+      arg_fourcc = makeFourCC(opt_fourcc);
+      if( isEmpty(arg_fourcc) ) {
+        fprintf(stderr, "ERROR: Invalid FourCC \"%s\"!\n", opt_fourcc);
+        return false;
+      }
+    } else {
+      fprintf(stderr, "ERROR: Invalid option \"%s\"!\n", argv[opt]);
+      return false;
+    }
+  }
+
+  if( opt >= argc ) {
+    return false;
+  }
+
+  arg_filename = argv[opt];
+
+  return true;
+}
+
+void usage(const char *prog)
+{
+  fprintf(stderr, "Usage: %s [--rip=<FourCC>] <input-filename>\n", prog);
+}
+
 int main(int argc, char **argv)
 {
+  // (1) Parse Arguments /////////////////////////////////////////////////////
+
+  if( !parseArgs(argc, argv) ) {
+    usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // (2) File I/O ////////////////////////////////////////////////////////////
+
   cs::File file;
-  if( !file.open(argv[1]) ) {
+  if( !file.open(arg_filename) ) {
+    fprintf(stderr, "ERROR: Unable to open file \"%s\"!\n", arg_filename);
+    usage(argv[0]);
     return EXIT_FAILURE;
   }
 
@@ -76,14 +130,6 @@ int main(int argc, char **argv)
 
   const Toc toc = Toc::read(buffer);
   toc.print();
-
-  const Block block1 = Block::read(buffer, Toc::SIZE_TOC);
-  block1.print();
-
-  const Block block2 = Block::read(buffer, block1.next());
-  block2.print();
-
-  extractStream("output.h265", buffer, toc.id_stream[0], FourCC{'H', '2', '6', '5'});
 
   return EXIT_SUCCESS;
 }
